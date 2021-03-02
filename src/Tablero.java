36728d8e521c5645b77a6c34da0d51e758df219e
JOSEANDRES14183 <jreyes14183@alumnes.iesmanacor.cat>
2021-03-02 11:20:54 +0100

    creada clase tablero, enum de dificultades

--- src/Tablero.java
+++ src/Tablero.java
@@ -1,2 +1,21 @@
+import java.util.Random;
 public class Tablero {
+
+    private int tamaño[][];
+    private int minas;
+    private Dificultad dificultad;
+
+    public Tablero(Dificultad dificultad){
+        this.dificultad=dificultad;
+        tamaño = new int [this.dificultad.getFilas()][this.dificultad.getColumnas()];
+        minas = dificultad.getMinas();
+    }
+
+
+    public void generarMinas(){
+        Random random = new Random();
+        for(int i=0;i<minas;i++){
+
+        }
+    }
 }
